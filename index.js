9b1bba7260a561142c2d973a78536f0e6bcb620d
gavin stahl <stahlgavin@gmail.com>
2019-11-14 12:26:17 -0800

    task 1 complete, 10 passes

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() {
+    return this.stomach.splice(0, this.stomach.length);
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
